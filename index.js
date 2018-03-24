c11be8a0ca351d8d0f4330d5f35391c9eef73966
Bayley <cbayley@proforma.com>
2018-03-24 12:06:26 -0400

    Added a clickme button to /hello

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express().use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/hello', (req, res) => {
+  // res.json({ hello: 'world' })
+  res.send('<button>clickme</button>');
+});
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
