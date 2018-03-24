c7f4a03f2b954984714bd56a1ac2d89fd17021d5
Bayley <cbayley@proforma.com>
2018-03-24 14:20:45 -0400

    Reverted

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ app.post('/webhook', (req, res) => {
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function (entry) {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
 
-          // Gets the message. entry.messaging is an array, but 
-          // will only ever contain one message, so we get index 0
-          let webhook_event = entry.messaging[0];
-          console.log(webhook_event);
-      });
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
 
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
   } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 
 });
@@ -52,17 +52,17 @@ app.get('/webhook', (req, res) => {
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
 
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-          // Responds with the challenge token from the request
-          console.log('WEBHOOK_VERIFIED');
-          res.status(200).send(req);
-
-      } else {
-          // Responds with '403 Forbidden' if verify tokens do not match
-          res.sendStatus(403);
-      }
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      // res.status(200).send(req);
+      res.status(200).send('CHALLANGED');
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
 });
 
@@ -75,4 +75,4 @@ app.get('/hello', (req, res) => {
 
 // End: Aaron
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
