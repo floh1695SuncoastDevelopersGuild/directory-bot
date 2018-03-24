dff745ed9dd2b88e9cdff06e17ab827ea290776c
Bayley <cbayley@proforma.com>
2018-03-24 12:03:26 -0400

    Refactored express statement

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+'use strict';
+
+const APP_ID = '159041564812667';
+const PAGE_ACCESS_TOKEN = 'EAACQpbZC5pXsBAHsvd8mzcvRJh7KCJ7rmPxdQCQvWGfdPUG3ZA31BcaWvRkuz9ZAmsKmcsmoXSHYcJv24eNMWoUKZArjLMgEj0V1kJ15zuBHPTCqWp3JpNd6ZBG0s0fFYo23af66LZAL7trRNQwpHj9J5zcd5ZAeGP7RE827hDbFwZDZD';
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express().use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
