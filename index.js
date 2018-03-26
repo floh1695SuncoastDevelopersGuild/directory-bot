54f4d53c3377611dd9471bb12388ab43a6f12f60
Bayley <cbayley@proforma.com>
2018-03-26 16:53:56 -0400

    asdlfkajsdlfjklfj

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function callSendAPI(sender_psid, response) {
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
-  console.log('body:', body);
+  console.log('TAG:BODY:', body);
 
   // Checks this is an event from a page subscription
   if (body && body.object === 'page') {
@@ -53,60 +53,29 @@ app.post('/webhook', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-// app.get('/webhook', (req, res) => {
+app.get('/webhook', (req, res) => {
 
-//   let VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
+  let VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
 
-//   // Parse the query params
-//   let mode = req.query['hub.mode'];
-//   let token = req.query['hub.verify_token'];
-//   let challenge = req.query['hub.challenge'];
-//   // Checks if a token and mode is in the query string of the request
-//   if (mode && token) {
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
 
-//     // Checks the mode and token sent is correct
-//     if (token === VERIFY_TOKEN) { //mode === 'subscribe' && 
+    // Checks the mode and token sent is correct
+    if (token === VERIFY_TOKEN) { //mode === 'subscribe' && 
 
-//       // Responds with the challenge token from the request
-//       console.log('WEBHOOK_VERIFIED');
-//       console.log(req.body);
-//       res.status(200).send(challenge);
-//     } else {
-//       // Responds with '403 Forbidden' if verify tokens do not match
-//       res.sendStatus(403);
-//     }
-//   }
-// });
-
-app.post('/webhook', (req, res) => {
-
-  // Parse the request body from the POST
-  let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-
-      // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-    });
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      console.log(req.body);
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
-
 });
 
 app.get('/hello', (req, res) => {
