d2ca2da2860d80d4c7fccc05417552751006ba78
Bayley <cbayley@proforma.com>
2018-03-26 16:00:22 -0400

    Small change

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/webhook', (req, res) => {
   if (mode && token) {
 
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (token === VERIFY_TOKEN) { //mode === 'subscribe' && 
 
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
