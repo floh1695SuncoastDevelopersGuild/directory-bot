1997faac41130c3e6dd6ff66b36935d7ba677fd7
Bayley <cbayley@proforma.com>
2018-03-27 11:15:55 -0400

    Can we handle messages?

--- index.js
+++ index.js
@@ -18,6 +18,20 @@ app.get('/', (req, res) => res.render('pages/index'));
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
   console.log('handleMessage');
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -28,6 +42,15 @@ function handlePostback(sender_psid, received_postback) {
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
   console.log('callSendAPI');
+  function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  }
 }
 
 // Creates the endpoint for our webhook
