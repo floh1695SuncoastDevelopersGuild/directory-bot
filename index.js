400132cba6c9e70854cd3b90aa4845eef6389d56
Bayley <cbayley@proforma.com>
2018-03-27 11:26:42 -0400

    More logs :DDDD

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function callSendAPI(sender_psid, response) {
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
+    console.log(err, '\n',res, '\n', body);
     if (!err) {
       console.log('message sent!')
     } else {
